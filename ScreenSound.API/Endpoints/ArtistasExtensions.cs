66252d9fec58bc31966698e22d3c204c2782f110
Isabel Sleigman <sleimusic@gmail.com>
2024-01-28 13:53:38 -0300

    Criando endpoints

---
+++ ScreenSound.API/Endpoints/ArtistasExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScreenSound.Modelos;
+using ScreenSound.Service;
+
+namespace ScreenSound.API.Endpoints
+{
+    public static class ArtistasExtensions
+    {
+        public static void AddEndPointArtistas(this WebApplication app)
+        {
+            #region Endpoint Artistas
+            app.MapGet("/Artistas", ([FromServices] ServiceBase<Artista> service) =>
+            {
+                return Results.Ok(service.Listar());
+            });
+
+            app.MapGet("/Artistas/{nome}", ([FromServices] ServiceBase<Artista> service, string nome) =>
+            {
+                var artista = service.ListarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+
+                if (artista is null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(artista);
+            });
+
+            app.MapPost("/Artistas", ([FromServices] ServiceBase<Artista> service, [FromBody] Artista artista) =>
+            {
+                service.Adicionar(artista);
+                return Results.Ok(artista);
+            });
+
+            app.MapDelete("/Artistas/{id}", ([FromServices] ServiceBase<Artista> service, int id) =>
+            {
+                var artista = service.BuscarPor(a => a.Id == id);
+
+                if (artista is null)
+                {
+                    return Results.NotFound();
+                }
+                service.Excluir(artista);
+                return Results.NoContent();
+            });
+
+            app.MapPut("/Artistas", ([FromServices] ServiceBase<Artista> service, [FromBody] Artista artista) =>
+            {
+                var artistaParaAtualizar = service.BuscarPor(a => a.Id == artista.Id);
+
+                if (artistaParaAtualizar is null)
+                {
+                    return Results.NotFound();
+                }
+                artistaParaAtualizar.Nome = artista.Nome is null ? artistaParaAtualizar.Nome : artista.Nome;
+                artistaParaAtualizar.Bio = artista.Bio is null ? artistaParaAtualizar.Bio : artista.Bio;
+                artistaParaAtualizar.FotoPerfil = artista.FotoPerfil is null ? artistaParaAtualizar.FotoPerfil : artista.FotoPerfil;
+
+                service.Editar(artistaParaAtualizar);
+                return Results.Ok();
+            });
+            #endregion
+        }
+    }
+}
---
+++ ScreenSound.API/Endpoints/MusicasExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScreenSound.Modelos;
+using ScreenSound.Service;
+
+namespace ScreenSound.API.Endpoints
+{
+    public static class MusicasExtensions
+    {
+        public static void AddEndPointMusicas(this WebApplication app)
+        {
+            #region Endpoint Músicas
+            app.MapGet("/Musicas", ([FromServices] ServiceBase<Musica> service) =>
+            {
+                return Results.Ok(service.Listar());
+            });
+
+            app.MapGet("/Musicas/{nome}", ([FromServices] ServiceBase<Musica> service, string nome) =>
+            {
+                var musica = service.BuscarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+                if (musica is null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(musica);
+
+            });
+
+            app.MapPost("/Musicas", ([FromServices] ServiceBase<Musica> service, [FromBody] Musica musica) =>
+            {
+                service.Adicionar(musica);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/Musicas/{id}", ([FromServices] ServiceBase<Musica> service, int id) => {
+                var musica = service.BuscarPor(a => a.Id == id);
+                if (musica is null)
+                {
+                    return Results.NotFound();
+                }
+                service.Excluir(musica);
+                return Results.NoContent();
+
+            });
+
+            app.MapPut("/Musicas", ([FromServices] ServiceBase<Musica> service, [FromBody] Musica musica) => {
+                var musicaParaAtualizar = service.BuscarPor(a => a.Id == musica.Id);
+                if (musicaParaAtualizar is null)
+                {
+                    return Results.NotFound();
+                }
+                musicaParaAtualizar.Nome = musica.Nome is null ? musicaParaAtualizar.Nome : musica.Nome;
+                musicaParaAtualizar.AnoLancamento = musica.AnoLancamento is null ? musicaParaAtualizar.AnoLancamento : musica.AnoLancamento;
+
+                service.Editar(musicaParaAtualizar);
+                return Results.Ok();
+            });
+            #endregion
+        }
+    }
+}
