0e4ead5bd37bbfdaa2bacdcdc3282a205b20a437
Isabel Sleigman <sleimusic@gmail.com>
2024-01-28 13:54:00 -0300

    Configurando documentação com swagger

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,6 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
+using ScreenSound.API.Endpoints;
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using ScreenSound.Service;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication
+    .CreateBuilder(args);
+
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<ServiceBase<Artista>>();
+builder.Services.AddTransient<ServiceBase<Musica>>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.AddEndPointArtistas();
+app.AddEndPointMusicas();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
